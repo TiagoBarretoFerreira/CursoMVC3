713181fb375cc0f61b21e5d01fe43bbed5712f12
Tiago Barreto Ferreira <tiag_online@msn.com>
2023-02-03 15:21:49 -0300

    Adicionar arquivos de projeto.

---
+++ cursoMVC3/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cursoMVC3.Models
+{
+    public class Context : DbContext
+    {
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=cadcheck.mssql.somee.com; user Id=***;pwd=***; Database=cadcheck;");
+        }
+    }
+}
